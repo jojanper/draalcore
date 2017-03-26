9e1feaf5805ed0bd05e330f0a5e8a42958e15e29
Juha Ojanperä <juha.ojanpera@gmail.com>
2017-03-27 00:00:58 +0300

    User registration as application action, initial skeleton.

--- draalcore/auth/apps.py
+++ draalcore/auth/apps.py
@@ -1,6 +1,12 @@
-from django.apps import AppConfig
+from draalcore.app_config import BaseAppConfig
 
 
-class AuthConfig(AppConfig):
+class AuthConfig(BaseAppConfig):
     name = 'draalcore.auth'
     label = 'draalcore.auth'
+    display_name = 'auth'
+
+    def ready(self):
+        from .actions import RegisterUserAction
+
+        self.actions = [RegisterUserAction]
