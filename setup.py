22d4d97ed94b4164114b32581c3c0c1493671590
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-06-23 14:24:58 +0300

    Fix setup, part 2.

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ install_requires = [
     'django>=3.2.4,<3.3',
     'djangorestframework==3.12.4',
     'six>=1.16.0,<2dev',
-    'celery>=5.1.0,<5dev',
+    'celery==5.1.0',
     'httplib2>=0.19.1,<1dev',
     'google-api-python-client==1.6.5',
     'oauth2',
