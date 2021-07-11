1626b04b7cd3631173e1890f9e940c222da6235e
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-07-11 19:22:48 +0300

    Setup + CI build updates, part 5

--- setup.py
+++ setup.py
@@ -66,8 +66,7 @@ setup(
         # that you indicate whether you support Python 2, Python 3 or both.
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8'
+        'Programming Language :: Python :: 3.7'
     ],
 
     # What does your project relate to?
