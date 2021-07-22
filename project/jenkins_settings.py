e9548e7f04a295b781d84ebef1bb47b48dc71ea3
Juha Ojanperä <juha.ojanpera@gmail.com>
2021-07-22 11:15:23 +0300

    Remove jenkins settings (obsolete).

--- project/jenkins_settings.py
+++
@@ -1,6 +0,0 @@
-from test_settings import *  # noqa
-
-TEST_RUNNER = 'xmlrunner.extra.djangotestrunner.XMLTestRunner'
-TEST_OUTPUT_VERBOSE = True
-TEST_OUTPUT_DESCRIPTIONS = True
-TEST_OUTPUT_DIR = 'build/test_reports/xmlrunner'
